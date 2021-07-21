399751e64ac3a992a46c0d7d42dbb7ae7314d59f
Roman Hillje <roman.hillje@googlemail.com>
2021-07-21 12:17:12 +0200

    Make the number of cores being used in the workflow configurable
    Co-authored-by: Ruslan Forostianov <ruslan@thehyve.nl>

--- run_coloc_pipeline_opt.sh
+++ run_coloc_pipeline_opt.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+CORES="${CORES:-8}"
+
 bash 1_find_overlaps.sh
 python 2_generate_manifest.py
 
 python 3a_make_conditioning_commands.py --quiet
-python 3a_make_conditioning_commands.py | shuf | parallel -j 40
+python 3a_make_conditioning_commands.py | shuf | parallel -j $CORES
 
 python 3b_make_coloc_commands.py --quiet
-python 3b_make_coloc_commands.py | shuf | parallel -j 40
+python 3b_make_coloc_commands.py | shuf | parallel -j $CORES
 
 python 5_combine_results.py
 python 6_process_results.py
